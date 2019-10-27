1b730d32bb0bedef41072cc732ecaca137c47a91
sriramede9 <only4programming03@gmail.com>
2019-10-27 11:43:24 -0400

    added carousel template integrated with js,style and fonts

--- src/main/java/com/music/controller/HomeController.java
+++ src/main/java/com/music/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.music.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,5 +10,10 @@ public class HomeController {
 	@RequestMapping("/good")
 	public String testrun() {
 		return "hello";
+	}
+	
+	@GetMapping("/welcome")
+	public String homePage() {
+		return "welcome";
 	}
 }
